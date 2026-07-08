ee92d0aeb075a9498b861de5961dc2cfcf028527
MD Shish <134275347+MDShish@users.noreply.github.com>
2026-07-08 15:42:38 +0530

    Time: 0 ms (100%), Space: 19.2 MB (85.48%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.py
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.py
@@ -1,17 +1,36 @@
+# class Solution:
+#     def sumAndMultiply(self, n: int) -> int:
+#         digit_sum = 0
+#         t = str(n)
+#         l1 = []
+#         for e in t:
+#             if e != "0":
+#                 l1.append(e)
+#         if not l1:
+#             return 0
+#         for w in l1:
+#             digit_sum += int(w)
+#         res = int("".join(l1)) * digit_sum
+#         return res
+
+# =============================================================
+
 class Solution:
     def sumAndMultiply(self, n: int) -> int:
+        digits = []
         digit_sum = 0
-        t = str(n)
-        l1 = []
-        for e in t:
-            if e != "0":
-                l1.append(e)
-        if not l1:
+
+        for ch in str(n):
+            if ch != '0':
+                digits.append(ch)
+                digit_sum += int(ch)
+
+        if not digits:
             return 0
-        for w in l1:
-            digit_sum += int(w)
-        res = int("".join(l1)) * digit_sum
-        return res
+
+        number = int("".join(digits))
+        return number * digit_sum
+    
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
