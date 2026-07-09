c4f1a25747717b623adf28458c507b67e0e5b4a4
MD Shish <134275347+MDShish@users.noreply.github.com>
2026-07-10 03:24:56 +0530

    Time: 0 ms (100%), Space: 22.9 MB (60.26%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,12 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        hsh = Counter(nums)
+        # Sort items by frequency in descending order and extract keys
+        most_common = sorted(hsh.items(), key=lambda x: x[1], reverse=True)
+        result = [item[0] for item in most_common[:k]]
+        return result
+    
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
