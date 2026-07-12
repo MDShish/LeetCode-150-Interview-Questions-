977c498a80dc9033b523004f2e4ca8e36aeb0fbf
MD Shish <134275347+MDShish@users.noreply.github.com>
2026-07-13 00:06:45 +0530

    Time: 5 ms (61.84%), Space: 22.5 MB (99.23%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,9 +1,20 @@
+# class Solution:
+#     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+#         hsh = Counter(nums)
+#         # Sort items by frequency in descending order and extract keys
+#         most_common = sorted(hsh.items(), key=lambda x: x[1], reverse=True)
+#         result = [item[0] for item in most_common[:k]]
+#         return result
+    
+
+#     ==============================================================================
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         hsh = Counter(nums)
         # Sort items by frequency in descending order and extract keys
-        most_common = sorted(hsh.items(), key=lambda x: x[1], reverse=True)
-        result = [item[0] for item in most_common[:k]]
+        most_common = sorted(hsh, key=hsh.get, reverse=True)
+        result = most_common[:k]
         return result
     
 
