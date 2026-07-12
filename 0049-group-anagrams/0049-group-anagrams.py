73b47f1bea4f3136b292c58715c508f641bb3439
MD Shish <134275347+MDShish@users.noreply.github.com>
2026-07-12 23:55:17 +0530

    Time: 8 ms (92.38%), Space: 21.9 MB (79.3%) - LeetHub - Time: O, Space: O(?)

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,12 +1,24 @@
+# class Solution:
+#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+#         d = collections.defaultdict(list)
+#         for s in strs:
+#             count = [0]*26
+#             for letter in s:
+#                 count[ord(letter)- ord('a')]+=1
+#             d[tuple(count)].append(s)
+#         return list(d.values())
+
+
+
+# ====================================================
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        d = collections.defaultdict(list)
+        mp = defaultdict(list)
         for s in strs:
-            count = [0]*26
-            for letter in s:
-                count[ord(letter)- ord('a')]+=1
-            d[tuple(count)].append(s)
-        return list(d.values())
+            key = ''.join(sorted(s))
+            mp[key].append(s)
+        return list(mp.values())
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
