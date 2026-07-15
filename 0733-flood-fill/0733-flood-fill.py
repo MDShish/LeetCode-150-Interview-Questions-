6cb7e4202a23bbc4d396be22231e58e7b7d61480
MD Shish <134275347+MDShish@users.noreply.github.com>
2026-07-16 05:12:41 +0530

    Time: 0 ms (100%), Space: 19.9 MB (16.33%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,21 +1,51 @@
+# class Solution:
+#     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+#         R, C = len(image), len(image[0])
+#         old_color = image[sr][sc]
+#         if old_color == color:
+#             return image
+#         def dfs(r, c):
+#             if r < 0 or r >= R or c < 0 or c >= C:
+#                 return
+#             if image[r][c] != old_color:
+#                 return
+            
+#             image[r][c] = color
+
+#             dfs(r - 1, c)
+#             dfs(r + 1, c)
+#             dfs(r, c - 1)
+#             dfs(r, c + 1)
+#         dfs(sr, sc)
+#         return image
+
+
+# ================================================================================
+
+
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         R, C = len(image), len(image[0])
         old_color = image[sr][sc]
+
         if old_color == color:
             return image
+
         def dfs(r, c):
-            if r < 0 or r >= R or c < 0 or c >= C:
-                return
             if image[r][c] != old_color:
                 return
-            
+
             image[r][c] = color
-            
-            dfs(r - 1, c)
-            dfs(r + 1, c)
-            dfs(r, c - 1)
-            dfs(r, c + 1)
+
+            if r >= 1:
+                dfs(r - 1, c)
+            if r + 1 < R:
+                dfs(r + 1, c)
+            if c >= 1:
+                dfs(r, c - 1)
+            if c + 1 < C:
+                dfs(r, c + 1)
+
         dfs(sr, sc)
         return image
 
