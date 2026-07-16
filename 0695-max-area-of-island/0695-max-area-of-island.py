6863b04b35cf32b207531401720f93a0d185e3f8
MD Shish <134275347+MDShish@users.noreply.github.com>
2026-07-16 07:49:21 +0530

    Time: 31 ms (14.63%), Space: 19.5 MB (90.32%) - LeetHub

---
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -0,0 +1,36 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        visited = set()
+        maxArea = 0
+        rows, cols = len(grid), len(grid[0])
+
+        def bfs(r, c):
+            count = 1
+            q = deque()
+            visited.add((r, c))
+            q.append((r, c))
+
+            while q:
+                x, y = q.popleft()
+
+                directions = [[1,0],[-1,0],[0,1],[0,-1]]
+
+                for dr, dc in directions:
+                    nr, nc = x + dr, y + dc
+
+                    if (0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1 and (nr, nc) not in visited):
+                        count += 1
+                        visited.add((nr, nc))
+                        q.append((nr, nc))
+            return count
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1 and (r, c) not in visited:
+                    maxArea = max(maxArea, bfs(r, c))
+
+        return maxArea
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
