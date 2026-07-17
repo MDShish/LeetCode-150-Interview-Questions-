b08597024768abcec35d64b0ee73a865eee900ef
MD Shish <134275347+MDShish@users.noreply.github.com>
2026-07-17 05:56:24 +0530

    Time: 99 ms (84.71%), Space: 61.4 MB (52.91%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -2,7 +2,7 @@ class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         combs = []
 
-        def helper2(i, curComb, n, k):
+        def helper2(i, curComb):
             if len(curComb) == k:
                 combs.append(curComb.copy())
                 return
@@ -11,10 +11,10 @@ class Solution:
 
             for j in range(i, n + 1):
                 curComb.append(j)
-                helper2(j + 1, curComb, n, k)
+                helper2(j + 1, curComb)
                 curComb.pop()
 
-        helper2(1, [], n, k)
+        helper2(1, [])
         return combs
 
 # Synced seamlessly with LeetHub Pro
