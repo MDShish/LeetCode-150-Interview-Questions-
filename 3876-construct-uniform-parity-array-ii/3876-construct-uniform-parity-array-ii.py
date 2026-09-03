a993cbb6c77a8637d2f7342837bb8f85d40b8ec9
MD Shish <134275347+MDShish@users.noreply.github.com>
2026-09-04 00:02:08 +0530

    Time: 45 ms (70.73%), Space: 35.3 MB (73.17%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
@@ -0,0 +1,7 @@
+class Solution:
+    def uniformArray(self, A: list[int]) -> bool:
+        return not (min(A) ^ reduce(or_, A)) & 1
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
